255efe252fa3f4dfb23a0c356217738b504eb109
Jackweb-rgba <radohery09@gmail.com>
2024-12-05 15:49:43 +0100

    valeur par defaut palindrome

--- recursive.py
+++ recursive.py
@@ -1,12 +1,14 @@
+# Factorielle 
 def factorielle(n):
     if n != 0:
         return (n*factorielle(n-1));
     else:
         return 1
+# print(factorielle(3)); ==> 6
 
-def palindrome(n,i): 
-    i = int(i);
-    n = str(n);
+# Palindrome 
+def palindrome(n,i=0): 
+    n = n.lower();
     if i <= (len(n) // 2):
         if n[i] == n[len(n)-i-1]:
             return palindrome(n,i+1);
@@ -14,4 +16,5 @@ def palindrome(n,i):
             return False;
     else:
         return True;
-print(palindrome("rottar",0))
+# print(palindrome("ici")) ==> True
+
