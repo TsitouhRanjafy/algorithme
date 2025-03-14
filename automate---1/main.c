9bda66f259cf324a68920a38af0c27a8997e6ecd
TsitouhRanjafy <radohery09@gmail.com>
2025-03-14 18:29:15 +0100

    init main fonction

--- automate---1/main.c
+++ automate---1/main.c
@@ -3,8 +3,14 @@
 #include "header.h"
 
 int main(void){
+    const signed char etat_initial[] = { 0 };
+    const signed char etat_final[] = { 5 };
+    const signed char etat[5] = {0,2,3,4,5};
+    const char alphabet[2] = {'m', 'a'};
+
     signed char * etat_actuel;
-    etat_actuel = transition(3,'a');
+    char mot[] = "";
+
     
     printf("-------------- Ensemble d'etat----------------\n");
     printf("------------------ Alphabet ------------------\n");
@@ -12,6 +18,9 @@ int main(void){
     printf("------------ Ensemble d'etat final -----------\n");
     printf("------------Ensemble de transition -----------\n");
     afficheTransition();
+
+    printf("\nEntrer le mot>");scanf("%s",&mot);
+    printf("%s",mot);
 
 
     
