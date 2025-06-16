d13478008b498ad9d761449bfc22380bb76ca74b
TsitouhRanjafy <radohery09@gmail.com>
2025-06-16 20:11:36 +0300

    aleatoire number with rand()

---
+++ aleatoire_number/main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+int generateRandomNumber(int min, int max);
+
+int main(){
+
+    // printf("%d",time(NULL)); // time now, en timestamp
+    printf("%d",generateRandomNumber(600,800));
+    return EXIT_SUCCESS;
+}
+
+int generateRandomNumber(int min, int max){
+    if (min > max) {
+        min += max;
+        max = min - max;
+        min -= max;
+    }
+    srand(time(NULL));
+    return (rand() % (max - min + 1)) + min; // formule 
+}
