211ad8942dc61ca896b9243011dab2380d904aef
TsitouhRanjafy <radohery09@gmail.com>
2025-07-22 14:52:52 +0300

    refactor: dfs-graph to lib

--- dfs/graph/main.c
+++ dfs/graph/main.c
@@ -1,19 +1,6 @@
 #include<stdio.h>
 #include<stdbool.h>
-#define STB_DS_IMPLEMENTATION
-#include "../lib/stb_ds.h"
-
-typedef struct {
-    size_t key;
-    size_t *value;
-} KV_ITEM;
-
-typedef struct {
-    size_t key;
-    bool value;
-} KV_BOOL;
-
-void dfsRec(KV_ITEM *adj,KV_BOOL **visited, size_t *node,size_t node_n,size_t origin, size_t **res);
+#include "../dfs-graph.h"
 
 int main(){
     KV_BOOL *visited = NULL;
@@ -86,18 +73,4 @@ int main(){
     hmfree(visited);
     arrfree(res);
     return 0;
-}
-
-void dfsRec(KV_ITEM *adj, KV_BOOL **visited, size_t *node, size_t node_n, size_t origin, size_t **res){
-    
-    hmput(*visited,origin,1);
-    printf("%d ",origin);
-    arrput(*res,origin);
-    
-    size_t *tmp = hmget(adj,origin);
-    for (size_t j = 0; j < arrlen(tmp); j++){
-        if (!hmget(*visited,tmp[j])){
-            dfsRec(adj,visited,node,node_n,tmp[j],res);
-        }
-    }
 }
