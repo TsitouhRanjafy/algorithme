dca38358eee2a5b8228a5051de3c077741b1bb06
TsitouhRanjafy <radohery09@gmail.com>
2025-07-22 16:03:54 +0300

    WIP

---
+++ fleury/dfs-graph.h
@@ -0,0 +1,53 @@
+/**
+ * @file dfs-graph.h
+ * dfs for undirected graph
+ */
+
+#ifndef DFS_GRAPH
+#define DFS_GRAPH
+
+#include<stdio.h>
+#include<stdbool.h>
+#define STB_DS_IMPLEMENTATION
+#include "./lib/stb_ds.h"
+
+typedef struct {
+    size_t key;
+    size_t *value;
+} KV_ITEM;
+
+typedef struct {
+    size_t key;
+    bool value;
+} KV_BOOL;
+
+size_t *dfs(KV_ITEM *adj, size_t *node, size_t node_n, size_t origin);
+
+#ifdef DFS_IMPLEMENTATION
+
+
+
+void dfsRec(KV_ITEM *adj, KV_BOOL **visited, size_t *node, size_t node_n, size_t origin, size_t **res){
+    
+    hmput(*visited,origin,1);
+    arrput(*res,origin);
+    
+    size_t *tmp = hmget(adj,origin);
+    for (size_t j = 0; j < arrlen(tmp); j++){
+        if (!hmget(*visited,tmp[j])){
+            dfsRec(adj,visited,node,node_n,tmp[j],res);
+        }
+    }
+}
+
+size_t *dfs(KV_ITEM *adj, size_t *node, size_t node_n, size_t origin){
+    KV_BOOL *visited = NULL;
+    size_t *res = NULL;
+
+    dfsRec(adj,&visited,node,node_n,origin,&res);
+    hmfree(visited);
+    return res;
+}
+
+#endif
+#endif
--- fleury/fleury.c
+++ fleury/fleury.c
@@ -1,8 +1,69 @@
 #include<stdio.h>
+#define DFS_IMPLEMENTATION
+#include "dfs-graph.h"
 
 int main(){
+    KV_ITEM *adj = NULL;
+    size_t node[] = {0,1,2,3,4,5,6};
+
+
+    // init graph
+    // adj for 3
+    size_t *item = NULL;
+    arrput(item,0);
+    arrput(item,1);
+    hmput(adj,3,item);
+
+    // adj for 0
+    item = NULL;
+    arrput(item,3);
+    arrput(item,4);
+    hmput(adj,0,item);
+
+    // adj for 1
+    item = NULL;
+    arrput(item,3);
+    arrput(item,4);
+    hmput(adj,1,item);
+
+    // adj for 4
+    item = NULL;
+    arrput(item,0);
+    arrput(item,1);
+    arrput(item,2);
+    arrput(item,6);
+    hmput(adj,4,item);
+
+    // adj for 2
+    item = NULL;
+    arrput(item,4);
+    arrput(item,5);
+    hmput(adj,2,item);
+
+    // adj for 6
+    item = NULL;
+    arrput(item,4);
+    arrput(item,5);
+    hmput(adj,6,item);
+
+    // adj for 5
+    item = NULL;
+    arrput(item,2);
+    arrput(item,6);
+    hmput(adj,5,item);
+
+
+    size_t *dfs_res = dfs(adj,node,arrlen(node),node[0]);
+    for (size_t i = 0; i < arrlen(dfs_res); i++){
+        printf("%d",dfs_res[i]);
+    }
     
 
+    for (size_t i = 0; i < hmlen(adj); i++){
+        arrfree(adj[i].value);
+    }
+    hmfree(adj);
+    arrfree(dfs_res);
     return 0;
 }
 
