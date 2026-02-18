5411035f826fa9795ae53011dd054e836a971371
TsitouhRanjafy <radohery09@gmail.com>
2026-02-18 16:41:01 +0300

    delete automate

--- automate-1/temp.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEMP_H
-#define TEMP_H
-
-extern void insert(signed char * etat);
-extern void affiche();
-extern signed char temp[];
-extern unsigned cpt;
-extern void reinitialise();
-#endif
